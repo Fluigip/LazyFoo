e00603f289cae6371f4576f36c01985e583aa56c
Fluigip <fluigipanettieri@gmail.com>
2017-07-26 21:17:39 -0300

    Lesson 01 + Project setup

---
+++ lesson_02/clear.bat
@@ -0,0 +1,9 @@
+:: Mute the output
+@echo off
+
+:: Open the directory folder
+cd build
+
+:: Clear the output path
+del * /S /Q
+del ..\bin\* /S /Q
---
+++ lesson_02/run.bat
@@ -0,0 +1,8 @@
+:: Mute the output
+@echo off
+
+:: Open the directory folder
+cd bin
+
+:: Run program
+main.exe
